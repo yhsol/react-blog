a1b43fe6cef1ec571787580eea5365e9cfc0f372
loshy <loshy244110@gmail.com>
2020-05-28 15:37:24 +0900

    feat: login

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -33,7 +33,30 @@ export const register = async (ctx) => {
   }
 };
 
-export const login = async (ctx) => {};
+export const login = async (ctx) => {
+  const { username, password } = ctx.request.body;
+
+  if (!username || !password) {
+    ctx.status = 401;
+    return;
+  }
+
+  try {
+    const user = await User.findByUsername(username);
+    if (!user) {
+      ctx.status = 401;
+      return;
+    }
+    const valid = await user.checkPassword(password);
+    if (!valid) {
+      ctx.status = 401;
+      return;
+    }
+    ctx.body = user.serialize();
+  } catch (error) {
+    ctx.throw(500, error);
+  }
+};
 
 export const check = async (ctx) => {};
 
