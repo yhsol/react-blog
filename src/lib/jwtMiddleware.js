bcc2c04686fc2824981ef184f0813eb7dc57f4cb
loshy <loshy244110@gmail.com>
2020-06-02 11:52:52 +0900

    feat: user check and logout

--- src/lib/jwtMiddleware.js
+++ src/lib/jwtMiddleware.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
+import User from "../models/user";
 
-const jwtMiddleware = (ctx, next) => {
+const jwtMiddleware = async (ctx, next) => {
   const token = ctx.cookies.get("access_token");
   if (!token) return next();
   try {
@@ -10,6 +11,16 @@ const jwtMiddleware = (ctx, next) => {
       username: decoded.username
     };
     console.log(decoded);
+
+    const now = Math.floor(Date.now() / 100);
+    if (decoded.exp - now < 60 * 60 * 24 * 3.5) {
+      const user = await User.findById(decoded._id);
+      const token = user.generateToken();
+      ctx.cookies.set("access_token", token, {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+      });
+    }
     return next();
   } catch (error) {
     return next();
